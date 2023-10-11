1c26158b0c265f53e4fb558b42caa78d5c78325c
Alex Sendre <rares.sendre@gmail.com>
2023-10-10 21:13:38 -0500

    added buttons to either view authors or books on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
@@ -7,7 +9,15 @@ function Home() {
 
   return (
     <div className="text-center my-4">
-      <h1 className="mt-5 text-white">Welcome, {user.displayName}</h1>
+      <h1 className="mt-5 text-white">Welcome, {user.displayName}!</h1>
+      <div className="mt-3 d-flex justify-content-center" style={{ gap: '10px' }}>
+        <Link passHref href="/book/view">
+          <Button type="button" className="btn-warning">View Books</Button>
+        </Link>
+        <Link passHref href="/author/view">
+          <Button type="button" className="btn-warning">View Authors</Button>
+        </Link>
+      </div>
     </div>
   );
 }
